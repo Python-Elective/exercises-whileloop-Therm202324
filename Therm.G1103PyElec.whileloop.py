efd190369fdc5a47e7fe8c6503ddf9062aa9c4b3
Therm202324 <111735992+Therm202324@users.noreply.github.com>
2023-09-05 08:15:10 +0700

    Exercise 3 done

--- Therm.G1103PyElec.whileloop.py
+++ Therm.G1103PyElec.whileloop.py
@@ -14,12 +14,10 @@ while x <= 10:
 print(x - 2)
 
 #solve Evercise 3 done
-# end = 9
-# sum_of_numbers = 0
-# current_number = 1
-
-# while current_number <= end:
-#   sum_of_numbers += current_number
-#   current_number += 1
-
-# print("The sum of numbers from 1 to", end, "is:", sum_of_numbers)
+end = 9
+sum_of_numbers = 0
+current_number = 1
+while current_number <= end:
+ sum_of_numbers += current_number
+ current_number += 1
+print("The sum of numbers from 1 to", end, "is:", sum_of_numbers)
