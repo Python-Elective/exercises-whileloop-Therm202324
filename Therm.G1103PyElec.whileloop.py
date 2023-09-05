1abda3fcfdff8345bfd95f301a5be9effc1fd738
Therm202324 <111735992+Therm202324@users.noreply.github.com>
2023-09-05 07:30:29 +0700

    Update Therm.G1103PyElec.whileloop.py

--- Therm.G1103PyElec.whileloop.py
+++ Therm.G1103PyElec.whileloop.py
@@ -1,6 +1,13 @@
 #solve Exercise 1 done
+# x = 0
+# while x < 10:
+#    x = x + 2
+# print(x)
+# print("Goodbye!")
+
+#solve Exercise 2 done
+print("Hello!")
 x = 0
 while x < 10:
     x = x + 2
 print(x)
-print("Goodbye!")
