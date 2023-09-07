a9c738e2ce088789369987f960f32c23f66c91ad
Therm202324 <111735992+Therm202324@users.noreply.github.com>
2023-09-07 09:55:33 +0700

    Commit now

--- Therm.G1103PyElec.whileloop.py
+++ Therm.G1103PyElec.whileloop.py
@@ -1,15 +1,15 @@
 #solve Exercise 1 done
 number = 2
 while number <= 10:
-    print(number)
-    number += 2
+     print(number)
+     number += 2
 print("Goodbye!")
 
 # solve Exercise 2 done
 # number = 10
-# print("Hello!", end=" ")
+# print("Hello!")
 # while number >= 2:
-#     print(number, end=" ")
+#     print(number)
 #     number -= 2
 # print() 
 
