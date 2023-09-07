013c771fbe5346da6ed93d63664a0ae08377610d
Therm202324 <111735992+Therm202324@users.noreply.github.com>
2023-09-07 10:15:06 +0700

    Exercise 2

--- Therm.G1103PyElec.whileloop.py
+++ Therm.G1103PyElec.whileloop.py
@@ -1,17 +1,17 @@
 #solve Exercise 1 done
-number = 2
-while number <= 10:
-     print(number)
-     number += 2
-print("Goodbye!")
+# number = 2
+# while number <= 10:
+#      print(number)
+#      number += 2
+# print("Goodbye!")
 
 # solve Exercise 2 done
-# number = 10
-# print("Hello!")
-# while number >= 2:
-#     print(number)
-#     number -= 2
-# print() 
+number = 10
+print("Hello!")
+while number >= 2:
+     print(number)
+     number -= 2
+print() 
 
 #solve Evercise 3 done
 # end = 9
