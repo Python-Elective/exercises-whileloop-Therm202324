ff4089d0285d2a8b7ecb9575c30663252f591387
Therm202324 <111735992+Therm202324@users.noreply.github.com>
2023-09-10 13:51:33 +0700

    Change placing

---
+++ Therm.G1103PyElec.whileloop.py
@@ -0,0 +1,23 @@
+#solve Exercise 1 done
+# number = 2
+# while number <= 10:
+#      print(number)
+#      number += 2
+# print("Goodbye!")
+
+# solve Exercise 2 done
+number = 10
+print("Hello!")
+while number >= 2:
+     print(number)
+     number -= 2
+print() 
+
+#solve Evercise 3 done
+# end = 9
+# sum_of_numbers = 0
+# current_number = 1
+# while current_number <= end:
+#  sum_of_numbers += current_number
+#  current_number += 1
+# print("The sum of numbers from 1 to", end, "is:", sum_of_numbers)
